9b73240935007d5c55f5c69883efabf0bd546466
madhumohan063 <madhumohan063@gmail.com>
2025-11-24 21:01:38 +0530

    Added Utility.java with areaOfCircle() and madhu() functions

---
+++ src/main/java/com/example/javamavenjunithelloworld/utility.java
@@ -0,0 +1,14 @@
+package com.example.javamavenjunitthelloworld;
+
+public class Utility {
+
+    // Your chosen function (Area of Circle Example)
+    public double areaOfCircle(double radius) {
+        return 3.141 * radius * radius;
+    }
+
+    // Your name function
+    public void madhu() {
+        System.out.println("I am Madhu");
+    }
+}
